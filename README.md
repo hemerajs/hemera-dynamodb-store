d12093edf5392d23d62eb83aa4f4a324388d0601
Nikola Kusibojovski <nikolakusibojovski@Nikolas-MacBook-Air-2.local>
2017-12-22 00:21:00 +0100

    First version of hemera-dynamodb-store

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# hemera-dynamodb-store
-Use DynamoDB with Hemera
+# Hemera-dynamodb-store package
+
+[![styled with prettier](https://img.shields.io/badge/styled_with-prettier-ff69b4.svg)](#badge)
+
+This is a plugin to use [DynamoDB] (https://aws.amazon.com/dynamodb/) with Hemera 
+
+## Install
+
+```
+npm i hemera-dynamodb-store --save
+```
+
+## Example
+
+```js
+hemera.use(require('hemera-dynamodb-store'), {
+   dynamodb: {
+        endpoint: 'http://localhost:8000',
+        region: 'eu-west-2'
+    }
+})
+
+hemera.ready(() => {
+  hemera.act({
+    topic: 'dynamo-store',
+    cmd: 'create',
+    collection: 'test',
+    data: {
+      id: '12345' , 
+      name: 'John Doe'
+    }
+  }, function (err, resp) {
+    this.log.info(resp, 'Query result')
+  })
+})
+
+```
+
+## Tests
+
+```
+npm run test
+```
+
+## Examples
+
+[Here] (https://aws.amazon.com/dynamodb/) you can find examples to see how to use it with Hemera 
---
+++ examples/dynamo-store.js
@@ -0,0 +1,114 @@
+'use strict'
+
+const Hemera = require('nats-hemera')
+const nats = require('nats').connect()
+const hemeraDynamo = require('hemera-dynamodb-store')
+
+const hemera = new Hemera(nats, {
+  logLevel: 'info',
+  childLogger: true
+})
+
+hemera.use(hemeraDynamo, {
+  dynamodb: {
+    endpoint: 'http://localhost:8000',
+    region: 'eu-west-2'
+  }
+})
+
+hemera.ready(() => {
+  hemera.act(
+    {
+      topic: 'dynamo-store',
+      cmd: 'create',
+      collection: 'test',
+      data: {
+        id: '1111',
+        name: 'John Doe'
+      }
+    },
+    function(err, resp) {
+      if (err) this.log.info(err, 'Errors occured')
+      this.log.info(resp, 'Query result')
+    }
+  )
+
+  hemera.act(
+    {
+      topic: 'dynamo-store',
+      cmd: 'removeById',
+      collection: 'test',
+      id: '1111'
+    },
+    function(err, resp) {
+      if (err) this.log.info(err, 'Errors occured')
+      this.log.info(resp, 'Query result')
+    }
+  )
+
+  hemera.act(
+    {
+      topic: 'dynamo-store',
+      cmd: 'updateById',
+      id: '1111',
+      collection: 'test',
+      UpdateExpression: 'set #city = :city, #country = :country',
+      ConditionExpression: '#name = :name',
+      ExpressionAttributeNames: { '#name': 'name', '#city': 'city', '#country': 'country'},
+      ExpressionAttributeValues: { ':name': 'John Doe', ':city': 'Skopje', ':country': 'Macedonia'}
+    },
+    function(err, resp) {
+      if (err) this.log.info(err, 'Errors occured')
+      this.log.info(resp, 'Query result')
+    }
+  )
+
+  hemera.act(
+    {
+      topic: 'dynamo-store',
+      cmd: 'findById',
+      collection: 'test',
+      id: '1111',
+      ProjectionExpression: '#name,#city',
+      ExpressionAttributeNames: {'#name': 'name', '#city': 'city'}
+    },
+    function(err, resp) {
+      if (err) this.log.info(err, 'Errors occured')
+      this.log.info(resp, 'Query result')
+    }
+  )
+
+  hemera.act(
+    {
+      topic: 'dynamo-store',
+      cmd: 'query',
+      collection: 'test',
+      KeyConditionExpression: '#id = :value',
+      FilterExpression: '#name = :name',
+      ProjectionExpression: '#name,#city',
+      ExpressionAttributeNames: {'#name': 'name', '#city': 'city', '#id': 'id'},
+      ExpressionAttributeValues: {':name': 'John Doe', ':value': '1111'}
+    },
+    function(err, resp) {
+      if (err) this.log.info(err, 'Errors occured')
+      this.log.info(resp, 'Query result')
+    }
+  )
+
+  hemera.act(
+    {
+      topic: 'dynamo-store',
+      cmd: 'scan',
+      collection: 'test',
+      FilterExpression: '#name = :name',
+      ProjectionExpression: '#name,#city',
+      ExpressionAttributeNames: {'#name': 'name', '#city': 'city'
+      },
+      ExpressionAttributeValues: {':name': 'John Doe', ':city': 'Skopje'}
+    },
+    function(err, resp) {
+      if (err) this.log.info(err, 'Errors occured')
+      this.log.info(resp, 'Query result')
+    }
+  )
+})
